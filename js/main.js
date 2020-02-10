93e57a8071c01b2b56b4a6c39293c9ed487f6eb2
Droideka <vindou@mail.ru>
2020-02-10 21:45:57 +0300

    Created vertical slider

--- js/main.js
+++ js/main.js
@@ -51,7 +51,6 @@ verticalSlider.addEventListener('mousemove', (e) => {
    if (!isDownVertical) return;
    e.preventDefault();
    const y = e.pageY - verticalSlider.offsetTop;
-   console.log(y);
    const walk = y - startY;
    verticalSlider.scrollTop = scrollTop - walk;
 });
@@ -59,12 +58,12 @@ verticalSlider.addEventListener('mouseleave', () => {
    isDown = false; 
 });
 
-// import data.json
+// import data.json 'Coming soon'
 
 const movieContainer = document.querySelector('.movie-container');
 const movieContent = document.querySelector('.movie-container__content');
-const url = `https://api.myjson.com/bins/p2dnz`;
-async function getData(url){
+const urlSoon = `https://api.myjson.com/bins/p2dnz`;
+async function getDataSoon(url){
    try {
       const response = await fetch(url);
       return await response.json();
@@ -72,7 +71,7 @@ async function getData(url){
       alert(err);
    }
 }
-getData(url).then(data => {
+getDataSoon(urlSoon).then(data => {
    data.forEach(item => {
       const element = createElement(item);
       slider.append(element);      
@@ -84,12 +83,18 @@ getData(url).then(data => {
 });
 
 function createElement(info){
-   const {title, year, poster, rank, likes_count, comments_count} = info;
+   const {title, year, poster, likes_count, comments_count} = info;
+   let {rank} = info;
+   if (!rank) rank = info.expectations_count || 0;
    const container = document.createElement('div');
    container.classList.add('slider__block');
-   const [myTitle, myMovie, myActions]  = [createTitle(title, year), createMovie(poster, title), createActions(comments_count, rank, likes_count)];
+   const [myTitle, myMovie, myActions] = 
+      [
+         createTitle(title, year), 
+         createMovie(poster, title), 
+         createActions(comments_count, rank, likes_count)
+      ];
    [myTitle,  myMovie, myActions].forEach(item => container.append(item));
-   // console.log(info);
    return container;
 }
 
@@ -200,6 +205,132 @@ function activeButtons(buttons){
    });
 }
 
+// import data.json 'Most popular'
 
+const popularContainer = document.querySelector('.vertical-inner');
+const urlPopular = `https://api.myjson.com/bins/p2dnz`;
+async function getDataPopular(url){
+   try {
+      const response = await fetch(url);
+      return await response.json();
+   } catch(err){
+      alert(err);
+   }
+}
+getDataPopular(urlPopular).then(data => {
+   data.forEach(item => {
+      const element = createElementPopular(item);
+      popularContainer.append(element);
+   });
+});
+
+function createElementPopular(info){
+   const {title, year, poster, likes_count, comments_count, director, writer, content} = info;
+   let {rank} = info;
+   if (!rank) rank = info.expectations_count || 0;
+   const container = document.createElement('div');
+   container.classList.add('vertical-container');
+   const block = document.createElement('div');
+   block.classList.add('vertical-container__block');
+   const [myPoster, myGrade, myPopularInfo] = 
+      [
+         createPoster(poster, title), 
+         createGrade(rank), 
+         createPopularInfo(title, year, director, writer, content, likes_count, rank, comments_count)
+      ];
+   [myPoster, myGrade, myPopularInfo].forEach(item => block.append(item));
+   container.append(block);
+   return container;
+}
+
+function createPoster(link, alt){
+   const posterContainer = document.createElement('div');
+   posterContainer.classList.add('vertical-container__poster');
+   const posterImage = document.createElement('img');
+   [posterImage.src, posterImage.alt] = [link, alt];
+   posterImage.classList.add('vertical-container__image');
+   const playButton = document.createElement('div');
+   playButton.classList.add('vertical-container__play');
+   const buttonImg = document.createElement('img');
+   [buttonImg.src, buttonImg.alt]  = [`../svg/play-button.svg`, `play`];
+   playButton.append(buttonImg);
+   posterContainer.append(posterImage);
+   posterContainer.append(playButton);
+   return posterContainer;
+}
+
+function createGrade(rank){
+   const rankContainer = document.createElement('div');
+   rankContainer.classList.add('vertical-container__rank');
+   const svg = document.createElement('object');   
+   svg.data = '../svg/circle.svg';
+   svg.type = 'image/svg+xml';
+   const text = document.createElement('p');
+   text.textContent = rank;
+   [svg, text].forEach(item => rankContainer.append(item));
+   return rankContainer;
+}
+
+function createPopularInfo(title, year, director, writer, content, likes, rank, comments){
+   const movieContainer = document.createElement('div');
+   movieContainer.classList.add('vertical-container__inner');
+
+   const movieTitle = document.createElement('h3');
+   movieTitle.classList.add('vertical-container__title');
+   movieTitle.textContent = title;
+
+   const movieInfo = document.createElement('h4');
+   movieInfo.classList.add('vertical-container__info');
+   movieInfo.textContent = `${year} | Director: ${director} | Writer: ${writer}`;
+
+   const movieText = document.createElement('p');
+   movieText.classList.add('vertical-container__text');
+   movieText.textContent = content;
+
+   const actions = createActions(comments, rank, likes);
+
+   const movieRead = document.createElement('a');
+   movieRead.href = '#';
+   movieRead.classList.add('vertical-container__link');
+   movieRead.textContent = `Read more`;
+
+   [movieTitle, movieInfo, movieText, actions, movieRead].forEach(item => {
+      movieContainer.append(item);     
+   });
+   return movieContainer;
+
+   function createActions(comments, ranks, likes){
+      const actionsContainer = document.createElement('ul');
+      actionsContainer.classList.add('vertical-container__actions');
+      let actionsButtons = ['share', 'comment', 'rank', 'like'];
+      actionsButtons = actionsButtons.map(name => {
+         const [block, count, img] = ['li', 'p', 'img'].map(item => document.createElement(item));
+         block.classList.add('vertical-container__actions-block');
+         switch(name){
+            case "share": {
+               count.textContent = '';    
+               img.src = `../svg/products/actions/${name}.svg`       
+            } break;
+            case "comment": {
+               count.textContent = comments || 0;  
+               img.src = `../svg/products/actions/${name}.svg`
+            } break;
+            case "rank": {
+               count.textContent = ranks || 0;  
+               img.src = `../svg/products/actions/${name}.svg`
+            } break;
+            case "like": {
+               count.textContent = likes || 0;  
+               img.src = `../svg/products/actions/${name}.svg`
+            } break;
+            default: count.textContent = '';  
+         }
+         [count, img].forEach(item => block.append(item));   
+         return block;
+      });
+      actionsButtons.forEach(item => actionsContainer.append(item));
+      return actionsContainer;
+   } 
+}
 
 
